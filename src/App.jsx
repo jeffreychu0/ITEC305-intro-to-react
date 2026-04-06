a492b450b592866b51b3791b5c5ea5b338e3a421
jeffreychu0 <jeffrey.chu06@gmail.com>
2026-04-06 12:14:39 -0400

    made custom component function with dynamic data

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+// jsx: javascript syntax extension
+
+// Goal: make a new component called header:
+
+import reactImg from './assets/react-core-concepts.png';
+
+function Header() {
+  const reactDescription = ['Important', 'Crucial', 'Core'];
+  const getRandomInt = (max) => {
+    return Math.floor(Math.random() * max);
+  }
+  
+  const description = reactDescription[getRandomInt(3)]
+
+  return (
+    <header>
+      <img src={reactImg} alt="Stylized atom" />
+      <h1>React Essentials</h1>
+      <p>
+        {description} Fundamental React concepts you will need for almost any app you are
+        going to build!
+      </p>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header/>
       <main>
         <h2>Time to get started!</h2>
       </main>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,10 @@
 import ReactDOM from "react-dom/client";
 
-import App from "./App.jsx";
+import App from "./App.jsx"; // jsx makes the components
 import "./index.css";
 
-const entryPoint = document.getElementById("root");
-ReactDOM.createRoot(entryPoint).render(<App />);
+const entryPoint = document.getElementById("root"); 
+ReactDOM.createRoot(entryPoint).render(<App />); // build app and put all of that code into the main
+
+// components need to have a capital letter (App) to prevent mixups with HTML syntax
+// Other rule: return renderable (jsx) content
